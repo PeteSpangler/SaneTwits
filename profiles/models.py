39874e4863ca626393f99730c36e2e1fae22674b
Pete Spangler <petertspangler@gmail.com>
2021-05-10 18:37:45 -0700

    Initial code uploaded

---
+++ profiles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class FollowerRelations(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=240, null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
+
+    def __str__(self):
+        return self.location
